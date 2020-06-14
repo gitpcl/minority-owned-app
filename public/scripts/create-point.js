05ff4cef387c7551de64f3aaab8f4ce917a29598
Pedro Lopes <pedrolopes@Pedros-MacBook-Pro.local>
2020-06-14 12:08:57 -0500

    Updated create-point.html CEP populate

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,55 +1,72 @@
-function populateUFs() {
-    const ufSelect = document.querySelector("select[name=uf]")
+// function populateUFs() {
+//     const ufSelect = document.querySelector("select[name=uf]")
 
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( res => res.json())
-    .then( states => {
+//     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+//     .then( res => res.json())
+//     .then( states => {
 
-        for( const state of states) {
+//         for( const state of states) {
 
-            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+//             ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
 
-        }
+//         }
 
-    } )
-}
+//     } )
+// }
 
-populateUFs()
+// populateUFs()
 
+// function getCities(event) {
+//     const citySelect = document.querySelector("select[name=city]")
+//     const stateInput = document.querySelector("input[name=state]")
 
+//     const ufValue = event.target.value
 
-function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
-    const stateInput = document.querySelector("input[name=state]")
+//     const indexOfSelectedState = event.target.selectedIndex
+//     stateInput.value = event.target.options[indexOfSelectedState].text
 
-    const ufValue = event.target.value
+//     const url =`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
-    const indexOfSelectedState = event.target.selectedIndex
-    stateInput.value = event.target.options[indexOfSelectedState].text
+//     citySelect.innerHTML = "<option value>Select a city</option>"
+//     citySelect.disabled = true
 
-    const url =`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-
-    citySelect.innerHTML = "<option value>Select a city</option>"
-    citySelect.disabled = true
-
-    fetch(url)
-    .then( res => res.json())
-    .then( cities => {
+//     fetch(url)
+//     .then( res => res.json())
+//     .then( cities => {
         
-        for( const city of cities) {
+//         for( const city of cities) {
 
-            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+//             citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
 
-        }
+//         }
 
-        citySelect.disabled = false
-    } )
+//         citySelect.disabled = false
+//     } )
+
+// }
+
+// document
+//     .querySelector("select[name=uf]")
+//     .addEventListener("change", getCities)
+
+//..................................//
+function populateCEPs() {
+    const cepSelect = document.querySelector('select[name=cep]')
+
+    let ceps = ['77002','77003', '77004']
+
+    cepSelect.addEventListener('click', () => {
+    let options = ceps.map(cep => `<options value=${cep()}>${cep}</options>`).join('\n');
+
+    cepSelect.innerHTML = options; 
+    })
 
 }
 
-document
-    .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
+populateCEPs()
+//..................................//
+
+//..................................//
 
 // Business type
 
